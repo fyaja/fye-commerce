87402625370707da998afdb2971822ac32f408d0
Ahmad Fay Baihaqi <ahmadfaybaihaqi@gmail.com>
2024-07-10 09:12:49 +0700

    fix some layout

--- script/ecommerce.js
+++ script/ecommerce.js
@@ -5,28 +5,30 @@ import { moneyFormat } from "./utils/money.js";
 function renderProducts() {
   let html = "";
   products.forEach((product) => {
-    html += `<div class="product">
-      <div class="product-top">
+    html += `
+    <div class="product">
+      <a href="productdetails.html?productId=${product.id}">
         <img src="${product.image}" alt="baju">
-        <div class="product-desc-item">
-          <div class="product-item">
+      </a>
+      <div class="product-item">
+        <a href="productdetails.html?productId=${product.id}">
+          <div class="product-desc-item">
             <h3 class="product-name">${product.name}</h3>
-            <p class="product-desc">${product.desc}</p>
             <p class="product-price">${moneyFormat(product.price)}</p>
+            <p class="product-desc">${product.desc}</p>
           </div>
+        </a>
 
-          <button class="add-to-wishlist">
-            <i class="ri-heart-line ri-xl"></i>
-          </button>
-        </div>
+        <button class="add-to-wishlist">
+          <i class="ri-heart-line ri-xl"></i>
+        </button>
       </div>
-
-      <button class="add-to-cart js-add-to-cart" data-product-id="${
-        product.id
-      }">
-        Add to cart
-      </button>
     </div>`;
+    // <button class="add-to-cart js-add-to-cart" data-product-id="${
+    //   product.id
+    // }">
+    //   Add to cart
+    // </button>;
   });
 
   document.querySelector(".js-products-container").innerHTML = html;
